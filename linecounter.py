857db7e6f204be2874e375f94216edd3df6829d0
Leonid Eremin <leremin@outlook.com>
2015-11-10 11:57:00 +0300

    fix gpl license

--- linecounter.py
+++ linecounter.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
+Copyright (C) 2015 Eremin V. Leonid (leremin@outlook.com)
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
